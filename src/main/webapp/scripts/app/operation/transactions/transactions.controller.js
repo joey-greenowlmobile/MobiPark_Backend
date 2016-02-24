e4d592fafaa66b3924aab1d33ce07f996ed60e8f
Ahmed Aly <ahmed.aly@greenowlmobile.com>
2016-02-24 14:18:41 -0500

    updated transactions page.
    1. Added sorting
    2. Removed Jquery DataTables.
    3. Added modal support for managing exceptions.
    4. Added gate opening support.

--- src/main/webapp/scripts/app/operation/transactions/transactions.controller.js
+++ src/main/webapp/scripts/app/operation/transactions/transactions.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('mainApp')
-    .controller('TransactionsController', function ($rootScope, $scope, $state, $timeout, $http, $log, TransactionService) {
+    .controller('TransactionsController', function ($rootScope, $scope, $state, $timeout, $http, $log, $modal, TransactionService) {
 
         $scope.records = [];
 
@@ -10,7 +10,112 @@ angular.module('mainApp')
             $scope.records = response;
         });
 
-        $(document).ready(function () {
-            $('#transactions_data_table').DataTable();
-        });
+        // Table sorting
+        $scope.sortType = 'id'; // set the default sort type
+        $scope.sortReverse = false;  // set the default sort order
+        $scope.searchRecord = '';     // set the default search/filter term
+
+        $scope.openModal = function (transaction) {
+            var modalInstance = $modal.open({
+                templateUrl: 'transactionTemplate.html',
+                controller: 'TransactionTemplateController',
+                resolve: {
+                    transaction: function () {
+                        return transaction;
+                    }
+                }
+            });
+            modalInstance.result.then(function (cb) {
+                return cb;
+            }, function () {
+                $log.info('Modal for transaction template dismissed at: ' + new Date());
+            });
+        };
+
     });
+
+
+angular.module('mainApp').controller('TransactionTemplateController', function ($rootScope, $scope, $state, $timeout, $modal, $modalInstance, $http, $log, transaction) {
+    $log.info("Opening transaciton template controller....");
+    $scope.record = transaction;
+
+    $scope.status = 0;
+
+    var getStatus = function () {
+        if ($scope.record.parkingStatus == null) {
+            return 0;
+        }
+        if ($scope.record.parkingStatus.indexOf("Parked") > -1) {
+            return 0;
+        }
+        if ($scope.record.parkingStatus.indexOf("Finished Parking") > -1) {
+            return 1;
+        }
+        if ($scope.record.parkingStatus.indexOf("COMPLETED") > -1) {
+            return 2;
+        }
+        if ($scope.record.parkingStatus.indexOf("EXCEPTION") > -1 || $scope.record.parkingStatus.indexOf("ERROR") > -1) {
+            return -1;
+        }
+        return 0;
+    };
+
+
+    $scope.openGateModal = function (transaction) {
+        var modalInstance = $modal.open({
+            templateUrl: 'openGateTemplate.html',
+            controller: 'OpenGateController',
+            resolve: {
+                transaction: function () {
+                    return transaction;
+                }
+            }
+        });
+        modalInstance.result.then(function (cb) {
+            return cb;
+        }, function () {
+            $log.info('Modal for transaction template dismissed at: ' + new Date());
+        });
+    };
+
+
+    $scope.openGate = function () {
+        $scope.cancel();
+        $scope.openGateModal($scope.record);
+    };
+
+    $scope.status = getStatus();
+    $scope.ok = function () {
+        $modalInstance.close(function () {
+            $log.info("Closing modal instance.");
+        });
+    };
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+
+});
+
+
+angular.module('mainApp').controller('OpenGateController', function ($rootScope, $scope, $state, $timeout, $modal, $modalInstance, $http, $log, transaction) {
+    $scope.record = transaction;
+
+    $scope.open = function () {
+        if (!confirm("Are you sure you want to open the gate for transaction id = " + $scope.record.id + " ?")) {
+            return;
+        }
+        alert("Succesfully opened gated.");
+    };
+
+
+    $scope.ok = function () {
+        $modalInstance.close(function () {
+            $log.info("Closing modal instance.");
+        });
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+
+});
