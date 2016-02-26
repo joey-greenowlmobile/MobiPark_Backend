e5d26f66a04e875e7eb1aa2c35135ee924a243ec
Ahmed Aly <ahmed.aly@greenowlmobile.com>
2016-02-26 10:09:04 -0500

    code clean up and versioning of two GateResource Apis.

--- src/main/java/com/greenowl/callisto/service/ParkingActivityService.java
+++ src/main/java/com/greenowl/callisto/service/ParkingActivityService.java
@@ -1,135 +1,128 @@
 package com.greenowl.callisto.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.inject.Inject;
-
-import org.joda.time.DateTime;
-import org.springframework.stereotype.Service;
-
 import com.greenowl.callisto.config.Constants;
 import com.greenowl.callisto.domain.ParkingActivity;
 import com.greenowl.callisto.domain.ParkingPlan;
-import com.greenowl.callisto.domain.ParkingSaleActivity;
 import com.greenowl.callisto.domain.User;
 import com.greenowl.callisto.repository.ParkingActivityRepository;
 import com.greenowl.callisto.repository.ParkingPlanRepository;
 import com.greenowl.callisto.web.rest.dto.ParkingActivityDTO;
-import com.greenowl.callisto.web.rest.dto.SalesActivityDTO;
+import org.joda.time.DateTime;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ParkingActivityService {
 
-	
-	@Inject
-	ParkingPlanRepository parkingPlanRepository;
-	@Inject
-	ParkingActivityRepository parkingActivityRepository;
-	
-	
 
-	private List<ParkingActivity> findAllActivityBetween(DateTime startTime, DateTime endTime) {
-		return parkingActivityRepository.getParkingActivityBetween(startTime, endTime);
-	}
-	
-	/**
-	 * Create parking activity for plan user and store in the database.
-	 * 
-	 * @param user
-	 * @param plan
-	 * @return
-	 */
-	public ParkingActivityDTO createParkingActivityForPlanUser(User user, ParkingPlan plan) {
-		ParkingActivity newActivity = new ParkingActivity();
-		newActivity.setActivityHolder(user);
-		newActivity.setLotId(plan.getLotId());
-		newActivity.setType("subscription");
-		Double totalCharge = 0.0;
-		newActivity.setParkingStatus(Constants.PARKING_STATUS_PARKING_START);
-		parkingActivityRepository.save(newActivity);
-		ParkingActivityDTO parkingActivityDTO = contructDTO(newActivity, user);
-		return parkingActivityDTO;
-	}
-	
-	/**
-	 * Return all the in-flight activities for the user.
-	 * 
-	 * @param user
-	 * @return
-	 */
-	public List<ParkingActivity> findInFlightActivityByUser(User user) {
-		List<ParkingActivity> parkingActivities = parkingActivityRepository.getParkingActivitiesByUser(user);
-		List<ParkingActivity> inFlightActivities = new ArrayList<ParkingActivity>();
-		for (ParkingActivity activity : parkingActivities) {
-			if (activity.getEntryDatetime() != null && activity.getExitDatetime() == null) {
-				inFlightActivities.add(activity);
-			}
-		}
-		return inFlightActivities;
-	}
-	
-	
+    @Inject
+    private ParkingPlanRepository parkingPlanRepository;
 
-	/**
-	 * Return all the activities between start date and end date based on the
-	 * filtered type.
-	 * 
-	 * @param startTime
-	 * @param endTime
-	 * @param type
-	 * @return
-	 */
-	public List<ParkingActivity> findAllFilteredActivityBetweenStartAndEndDate(DateTime start, DateTime end,
-			String type) {
-		List<ParkingActivity> filteredList = new ArrayList<>();
-		List<ParkingActivity> parkingActivities = findAllActivityBetween(start, end);
-		for (ParkingActivity activity : parkingActivities) {
-			switch (type.toLowerCase()) {
-			case "all":
-				filteredList.add(activity);
-				break;
-			case "inflight":
-				if (activity.getParkingStatus() != null) {
-					if (activity.getParkingStatus().equals("IN_FLIGHT")) {
-						filteredList.add(activity);
-					}
-				}
-				break;
-			case "park":
-				if (activity.getEntryDatetime() != null) {
-					if (activity.getParkingStatus().equals("COMPLETED")) {
-					filteredList.add(activity);
-					}
-				}
-				break;
-			case "exception":
-				if (activity.getParkingStatus() != null) {
-					if (activity.getParkingStatus().equals("EXCEPTION")) {
-						filteredList.add(activity);
-					}
-				}
-				break;
-			default:
-				break;
-			}
-		}
-		return filteredList;
-	}
-	
-	public ParkingActivityDTO contructDTO(ParkingActivity activity, User user){
-		return new ParkingActivityDTO(activity,user);
-	}
-	
-	public void updateParkingStatus(String parkingStatus, long id) {
-		parkingActivityRepository.setParkingStatusById(parkingStatus, id);
-	}
+    @Inject
+    private ParkingActivityRepository parkingActivityRepository;
 
-	public void updateGateResponse(String gateResponse, long id) {
-		parkingActivityRepository.setGateResponse(gateResponse, id);
-	}
 
-	public void updateExitTime(DateTime timestamp, long id) {
-		parkingActivityRepository.setExitTime(timestamp, id);
-	}
+    private List<ParkingActivity> findAllActivityBetween(DateTime startTime, DateTime endTime) {
+        return parkingActivityRepository.getParkingActivityBetween(startTime, endTime);
+    }
+
+    /**
+     * Create parking activity for plan user and store in the database.
+     *
+     * @param user
+     * @param plan
+     * @return
+     */
+    public ParkingActivityDTO createParkingActivityForPlanUser(User user, ParkingPlan plan) {
+        ParkingActivity newActivity = new ParkingActivity();
+        newActivity.setActivityHolder(user);
+        newActivity.setLotId(plan.getLotId());
+        newActivity.setType("subscription");
+        Double totalCharge = 0.0;
+        newActivity.setParkingStatus(Constants.PARKING_STATUS_PARKING_START);
+        parkingActivityRepository.save(newActivity);
+        ParkingActivityDTO parkingActivityDTO = contructDTO(newActivity, user);
+        return parkingActivityDTO;
+    }
+
+    /**
+     * Return all the in-flight activities for the user.
+     *
+     * @param user
+     * @return
+     */
+    public List<ParkingActivity> findInFlightActivityByUser(User user) {
+        List<ParkingActivity> parkingActivities = parkingActivityRepository.getParkingActivitiesByUser(user);
+        List<ParkingActivity> inFlightActivities = new ArrayList<ParkingActivity>();
+        for (ParkingActivity activity : parkingActivities) {
+            if (activity.getEntryDatetime() != null && activity.getExitDatetime() == null) {
+                inFlightActivities.add(activity);
+            }
+        }
+        return inFlightActivities;
+    }
+
+
+    /**
+     * Return all the activities between start date and end date based on the
+     * filtered type.
+     *
+     * @param type
+     * @return
+     */
+    public List<ParkingActivity> findAllFilteredActivityBetweenStartAndEndDate(DateTime start, DateTime end,
+                                                                               String type) {
+        List<ParkingActivity> filteredList = new ArrayList<>();
+        List<ParkingActivity> parkingActivities = findAllActivityBetween(start, end);
+        for (ParkingActivity activity : parkingActivities) {
+            switch (type.toLowerCase()) {
+                case "all":
+                    filteredList.add(activity);
+                    break;
+                case "inflight":
+                    if (activity.getParkingStatus() != null) {
+                        if (activity.getParkingStatus().equals("IN_FLIGHT")) {
+                            filteredList.add(activity);
+                        }
+                    }
+                    break;
+                case "park":
+                    if (activity.getEntryDatetime() != null) {
+                        if (activity.getParkingStatus().equals("COMPLETED")) {
+                            filteredList.add(activity);
+                        }
+                    }
+                    break;
+                case "exception":
+                    if (activity.getParkingStatus() != null) {
+                        if (activity.getParkingStatus().equals("EXCEPTION")) {
+                            filteredList.add(activity);
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        return filteredList;
+    }
+
+    public ParkingActivityDTO contructDTO(ParkingActivity activity, User user) {
+        return new ParkingActivityDTO(activity, user);
+    }
+
+    public void updateParkingStatus(String parkingStatus, long id) {
+        parkingActivityRepository.setParkingStatusById(parkingStatus, id);
+    }
+
+    public void updateGateResponse(String gateResponse, long id) {
+        parkingActivityRepository.setGateResponse(gateResponse, id);
+    }
+
+    public void updateExitTime(DateTime timestamp, long id) {
+        parkingActivityRepository.setExitTime(timestamp, id);
+    }
 }
--- src/main/java/com/greenowl/callisto/web/rest/GateResource.java
+++ src/main/java/com/greenowl/callisto/web/rest/GateResource.java
@@ -12,6 +12,7 @@ import com.greenowl.callisto.service.ParkingValTicketStatusService;
 import com.greenowl.callisto.service.SalesActivityService;
 import com.greenowl.callisto.service.UserService;
 import com.greenowl.callisto.service.config.ConfigService;
+import com.greenowl.callisto.util.ApiUtil;
 import com.greenowl.callisto.web.rest.dto.ParkingActivityDTO;
 import com.greenowl.callisto.web.rest.dto.SalesActivityDTO;
 import com.greenowl.callisto.web.rest.parking.GateOpenRequest;
@@ -44,17 +45,26 @@ public class GateResource {
 
     @Inject
     private ConfigService configService;
-    
+
     @Inject
     private ParkingActivityService parkingActivityService;
 
-    private Logger logger = LoggerFactory.getLogger(GateResource.class);
+    private static final Logger LOG = LoggerFactory.getLogger(GateResource.class);
 
     @RequestMapping(value = "/enter",
             method = RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_VALUE)
     @Transactional(readOnly = false)
     public ResponseEntity<?> enterParkingLot(@PathVariable("apiVersion") final String apiVersion, @RequestBody GateOpenRequest req) {
+        if (ApiUtil.getVersion().getValue() == 2) {
+            LOG.info("Returning V2 for Enter Parking Lot API.");
+            return enterParkingLotV2(req);
+        }
+        LOG.info("Returning V1 for Enter Parking Lot API.");
+        return enterParkingLotV1(req);
+    }
+
+    private ResponseEntity<?> enterParkingLotV1(GateOpenRequest req) {
         User user = userService.getCurrentUser();
         if (salesActivityService.findInFlightActivityByUser(user).size() != 0) {
             return new ResponseEntity<>(genericBadReq("User already inside parking lot, can't open gate.", "/gate"),
@@ -96,11 +106,7 @@ public class GateResource {
         }
     }
 
-    @RequestMapping(value = "/enterV2",
-            method = RequestMethod.POST,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    @Transactional(readOnly = false)
-    public ResponseEntity<?> enterParkingLotV2(@PathVariable("apiVersion") final String apiVersion, @RequestBody GateOpenRequest req) {
+    private ResponseEntity<?> enterParkingLotV2(GateOpenRequest req) {
         User user = userService.getCurrentUser();
         if (parkingActivityService.findInFlightActivityByUser(user).size() != 0) {
             return new ResponseEntity<>(genericBadReq("User already inside parking lot, can't open gate.", "/gate"),
@@ -115,12 +121,12 @@ public class GateResource {
                         //open gate
                         final String result = openGate(1, parkingActivityDTO.getId().toString());
                         if (result != null && (result.contains(Constants.GATE_OPEN_RESPONSE_1) || result.contains(Constants.GATE_OPEN_RESPONSE_2))) {
-                        	parkingActivityService.updateParkingStatus(Constants.PARKING_STATUS_PENDING, parkingActivityDTO.getId());
+                            parkingActivityService.updateParkingStatus(Constants.PARKING_STATUS_PENDING, parkingActivityDTO.getId());
                             ticketStatusService.createParkingValTicketStatus(parkingActivityDTO.getId(), Constants.PARKING_TICKET_TYPE_ENTER, DateTime.now());
                             return new ResponseEntity<>(parkingActivityDTO, org.springframework.http.HttpStatus.OK);
                         } else {
-                        	parkingActivityService.updateParkingStatus(Constants.PARKING_STATUS_EXCEPTION, parkingActivityDTO.getId());
-                        	parkingActivityService.updateGateResponse(result, parkingActivityDTO.getId());
+                            parkingActivityService.updateParkingStatus(Constants.PARKING_STATUS_EXCEPTION, parkingActivityDTO.getId());
+                            parkingActivityService.updateGateResponse(result, parkingActivityDTO.getId());
                             return new ResponseEntity<>(genericBadReq("ERROR-Failed to open parking gate：" + result, "/gate"),
                                     org.springframework.http.HttpStatus.BAD_REQUEST);
                         }
@@ -142,16 +148,15 @@ public class GateResource {
         }
     }
 
-    
-    
+
     private String openGate(int gateId, String ticketNo) {
         parkgateCmdClient parkClient = new parkgateCmdClient("localhost", 2222);
         String response = parkClient.openGate(gateId, ticketNo);
-        Boolean simulateMode = configService.get(AppConfigKey.GATE_SIMULATION_MODE.name(), Boolean.class);
-        logger.info("GATE_SIMULATE_MODE:" + simulateMode);
-        if (simulateMode != null && simulateMode && response != null && response.contains("OPEN-GATE: NOT-PRESENT")) {
+        Boolean simulateMode = configService.get(AppConfigKey.GATE_SIMULATION_MODE.name(), Boolean.class, false);
+        LOG.info("GATE_SIMULATE_MODE:" + simulateMode);
+        if (simulateMode && response != null && response.contains("OPEN-GATE: NOT-PRESENT")) {
             response = "OK Response with 33 chars:'TICKET: T12345 OPEN-GATE: OPEN OK'";
-            logger.info("replace response with \"OK Response with 33 chars:'TICKET: T12345 OPEN-GATE: OPEN OK'\"");
+            LOG.info("replace response with \"OK Response with 33 chars:'TICKET: T12345 OPEN-GATE: OPEN OK'\"");
         }
         return response;
     }
@@ -161,6 +166,15 @@ public class GateResource {
             produces = MediaType.APPLICATION_JSON_VALUE)
     @Transactional(readOnly = false)
     public ResponseEntity<?> exitParkingLot(@PathVariable("apiVersion") final String apiVersion, @RequestBody GateOpenRequest req) {
+        if (ApiUtil.getVersion().getValue() == 2) {
+            LOG.info("Returning V2 for Exit Parking Lot API.");
+            return exitParkingLotV1(req);
+        }
+        LOG.info("Returning V1 for Exit Parking Lot API.");
+        return exitParkingLotV2(req);
+    }
+
+    private ResponseEntity<?> exitParkingLotV1(GateOpenRequest req) {
         User user = userService.getCurrentUser();
         if (salesActivityService.findInFlightActivityByUser(user).size() == 0) {
             return new ResponseEntity<>(genericBadReq("User not in the parking lot, can't open gate.", "/gate"),
@@ -186,12 +200,8 @@ public class GateResource {
         }
     }
 
-    
-    @RequestMapping(value = "/exitV2",
-            method = RequestMethod.POST,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    @Transactional(readOnly = false)
-    public ResponseEntity<?> exitParkingLotV2(@PathVariable("apiVersion") final String apiVersion, @RequestBody GateOpenRequest req) {
+
+    private ResponseEntity<?> exitParkingLotV2(GateOpenRequest req) {
         User user = userService.getCurrentUser();
         if (parkingActivityService.findInFlightActivityByUser(user).size() == 0) {
             return new ResponseEntity<>(genericBadReq("User not in the parking lot, can't open gate.", "/gate"),
@@ -201,13 +211,13 @@ public class GateResource {
         if (parkingActivity != null) {
             final String result = openGate(2, Long.toString(parkingActivity.getId()));
             if (result != null && (result.contains(Constants.GATE_OPEN_RESPONSE_1) || result.contains(Constants.GATE_OPEN_RESPONSE_2))) {
-            	parkingActivityService.updateParkingStatus(Constants.PARKING_STATUS_PENDING, parkingActivity.getId());
+                parkingActivityService.updateParkingStatus(Constants.PARKING_STATUS_PENDING, parkingActivity.getId());
                 ticketStatusService.createParkingValTicketStatus(parkingActivity.getId(), Constants.PARKING_TICKET_TYPE_EXIT, DateTime.now());
                 ParkingActivityDTO parkingActivityDTO = parkingActivityService.contructDTO(parkingActivity, user);
                 return new ResponseEntity<>(parkingActivityDTO, org.springframework.http.HttpStatus.OK);
             } else {
-            	parkingActivityService.updateParkingStatus(Constants.PARKING_STATUS_EXCEPTION, parkingActivity.getId());
-            	parkingActivityService.updateGateResponse(result, parkingActivity.getId());
+                parkingActivityService.updateParkingStatus(Constants.PARKING_STATUS_EXCEPTION, parkingActivity.getId());
+                parkingActivityService.updateGateResponse(result, parkingActivity.getId());
                 return new ResponseEntity<>(genericBadReq("ERROR-Failed to open parking gate:" + result, "/gate"),
                         org.springframework.http.HttpStatus.BAD_REQUEST);
             }
