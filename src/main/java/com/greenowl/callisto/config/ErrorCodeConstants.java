109a4456f9e28a0a24dec14712835f9981071113
Ahmed Aly <ahmed.aly@greenowlmobile.com>
2016-03-02 10:51:50 -0500

    Formatted file.

--- src/main/java/com/greenowl/callisto/config/ErrorCodeConstants.java
+++ src/main/java/com/greenowl/callisto/config/ErrorCodeConstants.java
@@ -1,26 +1,30 @@
 package com.greenowl.callisto.config;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
 public class ErrorCodeConstants {
-	public enum MobilePlatform {
-		IOS, ANDROID
-	}
+    public enum MobilePlatform {
+        IOS, ANDROID
+    }
 
-	private ErrorCodeConstants() {
-	}
+    private ErrorCodeConstants() {
+    }
 
-	public static final Integer REGISTER_USERNAME_TAKEN = 1;
-	public static final Integer REGISTER_PHONENUM_TAKEN = 2;
-	public static final Integer REGISTER_STRIPE_FAILED = 3;
-	public static final Integer REGISTER_PLAN_NOTFOUND = 4;
-	public static final Integer REGISTER_DATABASE_ERROR = 5;
+    public static final Integer REGISTER_USERNAME_TAKEN = 1;
+    public static final Integer REGISTER_PHONENUM_TAKEN = 2;
+    public static final Integer REGISTER_STRIPE_FAILED = 3;
+    public static final Integer REGISTER_PLAN_NOTFOUND = 4;
+    public static final Integer REGISTER_DATABASE_ERROR = 5;
 
-	public static final Integer GATE_USER_INSIDE_PARKING_LOT = 1;
-	public static final Integer GATE_OPEN_FAILED = 2;
-	public static final Integer GATE_DATABASE_ERROR = 3;
-	public static final Integer GATE_INCORRECT_PARKING_LOT = 4;
-	public static final Integer GATE_USER_UNSUBSCRIBED = 5;
-	public static final Integer GATE_RECORD_NOTFOUND = 6;
-	public static final Integer GATE_USER_NOT_INSIDE_PARKING_LOT = 7;
+    public static final Integer GATE_USER_INSIDE_PARKING_LOT = 1;
+    public static final Integer GATE_OPEN_FAILED = 2;
+    public static final Integer GATE_DATABASE_ERROR = 3;
+    public static final Integer GATE_INCORRECT_PARKING_LOT = 4;
+    public static final Integer GATE_USER_UNSUBSCRIBED = 5;
+    public static final Integer GATE_RECORD_NOTFOUND = 6;
+    public static final Integer GATE_USER_NOT_INSIDE_PARKING_LOT = 7;
 
-	public static final Integer SUBSCRIPITION_DATABASE_ERROR = 3;
+    public static final Integer SUBSCRIPITION_DATABASE_ERROR = 3;
+
 }
